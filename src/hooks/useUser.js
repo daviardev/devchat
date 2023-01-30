f3989fab3aaae0439904c3d62ca420369b27a22c
devsilva <jerson.silva99@hotmail.com>
2023-01-29 19:07:10 -0500

    feat: Create hook for detect auth

---
+++ src/hooks/useUser.js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+import { authStateChanged } from 'firebase/client'
+
+export const STATES = {
+    NOT_LOGGED: null,
+    NOT_KNOWN: undefined
+}
+
+const useUser = () => {
+    const [user, setUser] = useState(STATES.NOT_KNOWN)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        authStateChanged(setUser)
+    }, [])
+
+    useEffect(() => {
+        user === STATES.NOT_LOGGED && router.push('/')
+    }, [user])
+
+    return user
+}
+
+export default useUser
