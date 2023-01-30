d3f7fe495b4a1193a06b684f9211edccc727d8ac
devsilva <jerson.silva99@hotmail.com>
2023-01-29 19:06:53 -0500

    feat: Create auth, state auth and map info of users

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 
-import { getAuth, GithubAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth'
+import { getAuth, GithubAuthProvider, onAuthStateChanged, signInWithPopup, useDeviceLanguage } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_APIKEY,
@@ -13,3 +13,32 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+const mapUserFromFirebaseAuth = user => {
+    user = auth.currentUser
+
+    const username = user.displayName
+    const avatar = user.photoURL
+    const uid = user.uid
+
+    return {
+        username,
+        avatar,
+        uid
+    }
+}
+
+export const authStateChanged = onChange => {
+    return onAuthStateChanged(auth, user => {
+        const normalizeUser = user ? mapUserFromFirebaseAuth(user) : null
+        onChange(normalizeUser)
+    })
+}
+
+export const loginWithGithub = async () => {
+    const githubProvider = new GithubAuthProvider()
+    useDeviceLanguage(auth)
+    return await signInWithPopup(auth, githubProvider)
+}
